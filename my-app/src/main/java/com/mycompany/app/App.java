4bbce5ea17fe8bb6aa544ea4d2315f918d5523f9
mrgrassho <mr.grassho@gmail.com>
2019-09-02 16:50:31 -0300

    Arregla plugin para ejecutar JFLEX.
    + Modo de ejecucion: mvn jflex:generate

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,10 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Lexer L = New Lexer();
     }
 }
